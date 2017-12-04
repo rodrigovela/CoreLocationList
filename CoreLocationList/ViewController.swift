8323f47c5c5db4de45fadbae14c2a8abee6d3b88
rodrigo <rodvela8292@gmail.com>
2017-12-04 15:14:32 -0600

    Load, Save, Delete from CoreData

--- CoreLocationList/ViewController.swift
+++ CoreLocationList/ViewController.swift
@@ -7,24 +7,60 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    // MARK: PROPERTIES
+    
     @IBOutlet weak var tableView: UITableView!
     var names: [String] = []
+    var actions: [NSManagedObject] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         title = "To Do List"
+        navigationItem.leftBarButtonItem = editButtonItem
         tableView.delegate = self
         tableView.dataSource = self
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        load()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // MARK: IBACTION
+    
+    @IBAction func add(_ sender: UIBarButtonItem) {
+        
+        let alert = UIAlertController(title: "New Name", message: "Add new name", preferredStyle: .alert)
+        let save = {(action: UIAlertAction) -> Void in
+            guard let textField = alert.textFields?.first, let name = textField.text else {
+                return
+            }
+            //self.names.append(name)
+            self.save(name: name)
+            self.tableView.reloadData()
+        }
+        let saveAction = UIAlertAction(title: "Save", style: .default, handler: save)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default, handler: nil)
+        alert.addTextField()
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
 
     // MARK: UITableViewDelegate
     
@@ -33,17 +69,83 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return names.count
+        return actions.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel?.text = names[indexPath.row]
+        let action = actions[indexPath.row]
+        cell.textLabel?.text = action.value(forKey: "name") as? String
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            delete(action: actions[indexPath.row])
+            actions.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }  else if editingStyle == .insert {
+    // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
     }
     
     /*func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         <#code#>
     }*/
+    
+    // MARK: Private Methods
+    
+    private func save(name: String) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        let entity = NSEntityDescription.entity(forEntityName: "Action", in: managedContext)
+        
+        let action = NSManagedObject(entity: entity!, insertInto: managedContext)
+        
+        action.setValue(name, forKeyPath: "name")
+        
+        do {
+            try managedContext.save()
+            actions.append(action)
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+    }
+    
+    private func load(){
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Action")
+        
+        do {
+            actions = try managedContext.fetch(fetchRequest)
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+    }
+    
+    private func delete(action: NSManagedObject) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        let managedContext = appDelegate.persistentContainer.viewContext
+        managedContext.delete(action)
+        do {
+            try managedContext.save()
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+    }
 }
 
